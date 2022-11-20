5416e7af723a314c05abf0d8cc60707581799915
oktvy <oktaynm@code.edu.az>
2022-11-20 07:55:49 -0800

    Student

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿using Student__s;
+using System.Globalization;
+using System.Net.Http.Headers;
+using System.Xml.Linq;
+
+Student student = new Student("Oktay", "Mammadli", "P230", 85, true);
+Console.WriteLine(student.Name);
+Student student1 = new Student("Samir", "Mamedov", "P381", 60, false);
+Console.WriteLine(student1.Point);
+
---
+++ Student.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Student__s
+{
+    internal class Student
+    {
+        public string Name;
+        public string Surname;
+        public string Group;
+        public byte Point;
+        public bool isGraduated;
+
+      
+
+        public Student(string name, string surname, string group, byte point, bool IsGraduated)
+        {
+            Name = name;
+            Surname = surname;
+            Group = group;
+            Point = point;
+            isGraduated = IsGraduated;
+
+        }
+        public void  Pointt()
+        {
+            if (Point > 80) 
+            {
+                Console.WriteLine("Imtahana icaze verilir");
+            }
+            else
+            {
+                Console.WriteLine("Imtahana icaze verilmir");
+            }
+        }
+
+
+    }
+}
